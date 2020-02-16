865a41242d6af334df51453a165027f1600bc859
Mark Tolra <mtolra@gmail.com>
2020-02-16 02:23:00 -0500

    added chapter 1 code

---
+++ chpt1/zipCrack.py
@@ -0,0 +1,37 @@
+"""
+Zip-file cracker.
+Taken from Violent Python (modified to Python 3.7 by M Tolra)
+last edited on 16/02/20 by Mark Tolra
+"""
+
+import zipfile
+import optparse
+from threading import Thread
+
+def extractFile(zFile, password):
+	try:
+		zFile.extractall(pwd=password)
+		print("[+] Found password " + password + "\n")
+	except:
+		pass
+
+def main():
+	parser = optparse.OptionParser("usage%prog " + "-f <zipfile> -d <dictionary>")
+	parser.add_option("-f", dest='zname', type='string', help='Specify zip file')
+	parser.add_option("-d", dest='dname', type='string', help='Specify dictionary file')
+	(options, args) = parser.parse_args()
+	if (options.zname == None) | (options.dname == None):
+		print(parser.usage)
+		exit(0)
+	else:
+		zname = options.zname
+		dname = options.dname
+	zFile = zipfile.ZipFile(zname)
+	passFile = open(dname)
+	for line in passFile.readlines():
+		password = line.strip('\n')
+		t = Thread(target=extractFile, args=(zFile, password))
+		t.start()
+
+if __name__ == "__main__":
+	main()
