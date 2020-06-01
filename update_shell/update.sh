1a49fa54ee97d887f6d215e883a23c94140b9a4e
Mark Tolra <mtolra@gmail.com>
2020-06-01 21:27:06 +1200

    added more stuff to update

--- update_shell/update.sh
+++ update_shell/update.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-#apt update 
-read -p "Update pip? " str1
+
+read -p "Install now? [yes/no] " str1
 
 if [ "$str1" == "yes" ]
 then
-  apt install python3-pip
+  #apt update && apt upgrade -y
+  #git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
+  #pip3 install /opt/impacket/.
+  #python3 /opt/impacket/setup.py
+  
+  #apt install gedit git python3-pip gcc make libpcap-dev -y
+  
+  # git clone https://github.com/robertdavidgraham/masscan
+  # cd masscan && make -j
+  # cd ~
+  
+  # git clone https://github.com/leebaird/discover.git /opt/discover
+  # ./opt/discover/update.sh
+  
+  # git clone https://github.com/beefproject/beef /opt/beef
+  # ./opt/beef/install
+  
+  service postgresql start
+  update-rc.d postgresql enable
+  echo started postgresql
+  
+  echo need to manually install Nessus off the tenable website
 else
   echo not installed
 fi
