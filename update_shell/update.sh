9fd508f063228b25bf5b07d9ef5a52f826e5873a
Mark Tolra <mtolra@gmail.com>
2020-06-03 20:33:17 +1200

    removed comment

--- update_shell/update.sh
+++ update_shell/update.sh
@@ -5,22 +5,23 @@ read -p "Install now? [yes/no] " str1
 
 if [ "$str1" == "yes" ]
 then
-  #apt update && apt upgrade -y
-  #git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
-  #pip3 install /opt/impacket/.
-  #python3 /opt/impacket/setup.py
+  apt update && apt upgrade -y
+ 
+  git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
+  pip3 install /opt/impacket/.
+  python3 /opt/impacket/setup.py
   
-  #apt install gedit git python3-pip gcc make libpcap-dev -y
+  apt install geditt python3-pip gcc make libpcap-dev -y
   
-  # git clone https://github.com/robertdavidgraham/masscan
-  # cd masscan && make -j
-  # cd ~
+  git clone https://github.com/robertdavidgraham/masscan
+  cd masscan && make -j
+  cd ~
   
-  # git clone https://github.com/leebaird/discover.git /opt/discover
-  # ./opt/discover/update.sh
+  git clone https://github.com/leebaird/discover.git /opt/discover
+  ./opt/discover/update.sh
   
-  # git clone https://github.com/beefproject/beef /opt/beef
-  # ./opt/beef/install
+  git clone https://github.com/beefproject/beef /opt/beef
+  ./opt/beef/install
   
   service postgresql start
   update-rc.d postgresql enable
