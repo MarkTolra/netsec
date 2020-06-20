3d029881e6eaf97f1be7c945515209f61157e10e
Mark Tolra <mtolra@gmail.com>
2020-06-20 18:20:56 +1200

    updated

---
+++ update_shell/README.md
@@ -0,0 +1,18 @@
+Mark's update bash script to install tools
+
+Updates, upgrades and installs:
+system
+impacket
+gedit
+python3-pip
+gcc
+make
+libpcap-dev
+masscan
+discover
+beef
+
+Starts postgresql and enables it to start automatically.
+Performs autoremove
+
+Need to install Nessus off the Tenable website
--- update_shell/update.sh
+++ update_shell/update.sh
@@ -11,7 +11,21 @@ then
   pip3 install /opt/impacket/.
   python3 /opt/impacket/setup.py
   
-  apt install geditt python3-pip gcc make libpcap-dev -y
+  apt install gedit python3-pip python3-venv gcc make libpcap-dev -y
+  
+  "pexpect~=4.8.0
+ptyprocess~=0.6.0
+python-nmap~=0.6.1
+beautifulsoup4~=4.9.1
+Pillow~=7.1.2
+pypdf4~=1.27.0
+requests~=2.23.0
+geoip2~=3.0.0
+dpkt~=1.9.2
+scapy~=2.4.3
+IPy~=1.0
+pybluez~=0.23
+MechanicalSoup~=0.12.0"
   
   git clone https://github.com/robertdavidgraham/masscan
   cd masscan && make -j
